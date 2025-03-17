851a1f4962ef0b8d14b1b7a67fff81775c11ce02
Ramcharan10122005 <kundenaramcharan@gmail.com>
2025-03-18 04:42:11 +0530

    added hyd

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ async function connectDB() {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST || 'localhost',
             user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASS || 'Root@123',
+            password: process.env.DB_PASS || 'Ramcharan@1104',
             database: process.env.DB_NAME || 'bus'
         });
 
